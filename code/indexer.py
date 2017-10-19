407ddd8b2c3e6b312e5c0fe5c122d2c072e20856
ChaohuiYu <yuchaohui1234@gmail.com>
2017-10-19 13:42:17 +0800

    1.Add singlestringcompress to indexer.py 2.Resolve some bugs like: it will stuck into a while when get a next document.

--- code/indexer.py
+++ code/indexer.py
@@ -1,19 +1,19 @@
 # coding=utf-8
 import os
 from tokenstream import TokenStream
+from singlestringcomp import Singlestringcomp
 
 class indexer:
     def __init__(self, data_path, max_block=10000):
         self.dir_path = os.path.dirname(data_path)
-        self.block_dir = os.path.join(data_path, 'indexer_block')
+        self.block_dir = os.path.join(self.dir_path, 'indexer_block')
         self.block_path_repo = []
         self.max_block = max_block
         """ 判断词项流是否为空 """
         self.empty_stream = False
         """分词"""
         self.tokenstream = TokenStream(data_path)
-
-
+        return 
 
     # SPIMI算法构建索引块
     def spimi_invert(self):
@@ -29,6 +29,7 @@ class indexer:
             if tmp_term_pos_docid is None:
                 self.empty_stream = True
                 break
+
             """ 返回不是None """
             term,term_pos,docid = tmp_term_pos_docid
             """ 不在词典中，加入并返回在词典中的位置 """
@@ -40,13 +41,23 @@ class indexer:
                 """ 返回在词典中位置 """
                 pos = term_dic[term]
                 """ 词项所在文档ID不在倒排记录表里 """
-                if docid != inverted_index[pos][-1]
+                if docid != inverted_index[pos][-1]:
                     inverted_index[pos].append(docid)
             block_size += 1
         return term_dic, term_cnt, inverted_index
 
     # 索引块持久化
-    def write_index_block(self, block_path):
+    def write_index_block(self, block_path, term_dic, inverted_index):
+        """ 对字典排序 """
+        term_dic_sorted = [(k, term_dic[k]) for k in sorted(term_dic.keys())]
+        temp_lst = [0]*len(term_dic_sorted)
+        for i,(k, v) in enumerate(term_dic_sorted):
+            """ 将排序后的字典元素放到新list里 """
+            temp_lst[i] = inverted_index[v]
+        """ 对字典进行单一字符串压缩 和 持久化 """
+        Singlestringcomp.write_dic(term_dic_sorted, block_path + '_sscompdic')
+        """ Gamma """
+         
 
 
 
@@ -55,17 +66,22 @@ class indexer:
         index_block = 0
         while True:
             """ 用SPIMI算法构建索引块 """
-            if self.empty_stream == True:
-                break
+            """ 拿到了一个索引块 """
             term_dic, term_cnt, inverted_index = self.spimi_invert()
             index_block += 1
+            """ ../../index_block_1/2/3... """
             self.block_path_repo.append(os.path.join(self.block_dir, 
-                'index_block_' + str(indexer_block)))
+                'index_block_' + str(index_block)))
             """ 索引块持久化 """
+            self.write_index_block(self.block_path_repo[-1], term_dic, inverted_index)
+
+            if self.empty_stream:
+                break
+            
             
 
     # 构建倒排记录表和词典
-    def build_indexer:
+    def build_indexer(self):
         """
         1.构建索引块,合并索引块,写入磁盘过程中压缩
         2.对每一个块产生独立的字典，和倒排索引
@@ -73,9 +89,11 @@ class indexer:
         """
         # 构建索引块
         self.build_index_block()
+        return 
 
 if __name__ == '__main__':
-    pass
+    idx = indexer('/home/superhui/Informationretrieval/IR/BigHW_IR/data/doc')
+    idx.build_indexer()
 
 
 
--- code/tokenstream.py
+++ code/tokenstream.py
@@ -4,9 +4,9 @@ import os
 import re
 
 class TokenStream:
-    def __init__(self, file_dir):
+    def __init__(self, data_dir):
         """ 读取文档，得到倒排记录表term - docid """
-        self.file_dir = file_dir
+        self.file_dir = data_dir
         self.file_names = os.listdir(self.file_dir)
         self.document = None
         self.documentid = 0
@@ -28,6 +28,14 @@ class TokenStream:
         for i,word in enumerate(token):
             term.append((word,i))
         return term
+    
+    # 获得语料库下一个文件路径
+    def get_next_file_path(self):
+        if len(self.file_names) is 0:
+            return None
+        file_path = os.path.join(self.file_dir, self.file_names[0])
+        self.file_names.pop(0)
+        return file_path
 
     # 获得下一篇文档 和 对应的词项
     def next_doc_and_term(self):
@@ -39,19 +47,21 @@ class TokenStream:
             self.document = self.document.next_sibling
         while type(self.document) is not bs4.element.Tag:
             if self.document is None:
-                if len(self.file_names) is 0:
-                    return None
-                if len(self.file_names) is not 0:
-                    file_path = os.path.join(self.file_dir, self.file_names[0])
-                    self.file_names.pop(0)
-                    with open(file_path, 'r') as doc_file:
-                        soup = bs4.BeautifulSoup(doc_file, features = 'lxml')
-                    self.document = soup.doc
+                file_path = self.get_next_file_path()
+                """ 语料库里的文档被读取完 """
+                if file_path is None:
+                    #return None
+                    break
+                with open(file_path, 'r') as doc_file:
+                    soup = bs4.BeautifulSoup(doc_file, features = 'lxml')
+                self.document = soup.doc
             else:
                 self.document = self.document.next_sibling
+
         """ 获得self.document 里的词项, 放在document_term """
         if self.document is None:
             self.document_term = None
+            return None
         else:
             self.documentid += 1
             docno_name = self.document.docno.string
@@ -59,7 +69,8 @@ class TokenStream:
             self.document_num[self.documentid] = docno_name
             """ 每一个文档转成 字符串 """
             temp = ''
-            for row in dd.document.docno.next_siblings:
+            """ 把文档转成词条 """
+            for row in self.document.docno.next_siblings:
                 if type(row) is bs4.element.Tag:
                     row = row.strings
                 if type(row) is not bs4.element.NavigableString:
@@ -74,14 +85,15 @@ class TokenStream:
 
     # 去读下一个词项
     def get_next_term(self):
+        """ 用来判断一篇文档的词是否读完 """
         self.termpos += 1 
         """
-        如果词项位置超过了一篇文档数
+        如果词项位置超过了一篇文档数,读完
         """
         while self.termpos >= len(self.document_term):
             self.next_doc_and_term()
             self.termpos += 1
-            if self.document_term == None:
+            if self.document_term is None:
                 return None
         return self.document_term[self.termpos]
     
@@ -92,19 +104,14 @@ class TokenStream:
             return None
         return (tmp_term_pos[0],tmp_term_pos[1],self.documentid)
     
-        
+       
     
 
 if __name__ == '__main__':
-    dd = TokenStream('/home/superhui/Informationretrieval/DocumentIndexer/data/document')
-    dd.next_doc_and_term()
-    print dd.document
-    print dd.get_next_term()
-    print dd.get_next_term()
-    for i in range(10000):
-        print dd.get_next_term_pos_docid()
-
-
-
-
-
+    dd = TokenStream('/home/superhui/Informationretrieval/IR/BigHW_IR/data/doc')
+    #dd.next_doc_and_term()
+    #print dd.document
+    #print dd.get_next_term()
+    #print dd.get_next_term()
+    #for i in range(10000):
+    #    print dd.get_next_term_pos_docid()
